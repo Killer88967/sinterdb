a671fd82f38b23d8dae7cd1a8ebc7eea0e69ae1f
Killer88967 <Erniebug27@gmail.com>
2026-09-22 13:07:06 -0400

    Refactor error handling in in-memory-collection
    Removed StorageError and StorageInsertManyError classes along with StorageErrorCode object. Updated imports to use StorageError and StorageErrorCode from './errors.js'.

--- packages/storage/src/in-memory-collection.ts
+++ packages/storage/src/in-memory-collection.ts
@@ -7,16 +7,11 @@ import {
   type DocumentValue,
 } from "sinterdb-protocol";
 
-export const StorageErrorCode = {
-  InvalidDocument: "INVALID_DOCUMENT",
-  InvalidDocumentId: "INVALID_DOCUMENT_ID",
-  InvalidFilter: "INVALID_FILTER",
-  InvalidBatch: "INVALID_BATCH",
-  DuplicateId: "DUPLICATE_ID",
-} as const;
-
-export type StorageErrorCode =
-  (typeof StorageErrorCode)[keyof typeof StorageErrorCode];
+import {
+  StorageError,
+  StorageErrorCode,
+  StorageInsertManyError,
+} from "./errors.js";
 
 export interface StorageInsertOneResult {
   readonly insertedId: CustomId;
@@ -29,44 +24,6 @@ export interface StorageInsertManyResult {
 interface CompiledFilterField {
   readonly name: string;
   readonly encodedValue: Uint8Array;
-}
-
-export class StorageError extends Error {
-  public readonly code: StorageErrorCode;
-
-  public constructor(
-    code: StorageErrorCode,
-    message: string,
-    options: ErrorOptions = {},
-  ) {
-    super(message, options);
-
-    this.name = "StorageError";
-    this.code = code;
-  }
-}
-
-export class StorageInsertManyError extends StorageError {
-  public readonly failedIndex: number;
-  public readonly insertedIds: readonly CustomId[];
-
-  public constructor(
-    failedIndex: number,
-    insertedIds: readonly CustomId[],
-    cause: StorageError,
-  ) {
-    super(
-      cause.code,
-      `Insert failed at batch index ${failedIndex}: ${cause.message}`,
-      {
-        cause,
-      },
-    );
-
-    this.name = "StorageInsertManyError";
-    this.failedIndex = failedIndex;
-    this.insertedIds = Object.freeze([...insertedIds]);
-  }
 }
 
 export class InMemoryCollection {
