32a83662044f11b1282c59977592ebb05dbce5ba
Killer88967 <Erniebug27@gmail.com>
2026-09-21 21:58:37 -0400

    Export StorageInsertManyError and StorageInsertManyResult

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -2,9 +2,11 @@ export {
   InMemoryCollection,
   StorageError,
   StorageErrorCode,
+  StorageInsertManyError,
 } from "./in-memory-collection.js";
 
 export type {
   StorageErrorCode as StorageErrorCodeValue,
+  StorageInsertManyResult,
   StorageInsertOneResult,
 } from "./in-memory-collection.js";
