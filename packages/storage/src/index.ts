699f80adba3638a15276d4ad41b58d41df15c636
Killer88967 <Erniebug27@gmail.com>
2026-09-22 13:48:12 -0400

    Import CompiledFilter type from filter.js

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -4,9 +4,11 @@ export {
   StorageErrorCode,
   StorageInsertManyError,
 } from "./errors.js";
+import { comileFilter } from "./filter.js";
 
 export type {
   StorageInsertManyResult,
   StorageInsertOneResult,
 } from "./in-memory-collection.js";
 export type { StorageErrorCode as StorageErrorCodeValue } from "./errors.js";
+import type { CompiledFilter} from "./filter.js";
